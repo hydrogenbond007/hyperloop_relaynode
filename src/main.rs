d149018664385f0a86aa5cc58e82fed2fa8182c7
Madhav <madhavgoyal007protonmail.com>
2024-03-22 20:28:05 +0530

    add helios and ethers filter

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[allow(unused_imports)]
 use std::{str::FromStr, env};
 
-use helios::{client::ClientBuilder, config::networks::Network, types::BlockTag};
-use ethers::{types::Address, utils};
+use helios::client::ClientBuilder;
+use helios::config::networks::Network;
+use helios::types::BlockTag;
+use helios::client::*;
+use helios::rpc::*;
+use ethers::types::{Address, Filter};
+use ethers::utils::*;
+#[allow(unused_imports)]
+use ethers::{types::Address, utils, FilterBuilder, H160, H256};
 #[allow(unused_imports)]
 use eyre::Result;
 #[allow(unused_imports)]
@@ -12,59 +19,40 @@ use web3::Web3;
 #[allow(unused_imports)]
 use web3::transports::Http;
 
-fn main() {
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            let untrusted_rpc_url = env::var("normal_rpc")?;
+async fn main() -> Result<()> {
+    let untrusted_rpc_url = env::var("private_rpc")?;
 
-            let mut client = ClientBuilder::new()
-                .network(Network::MAINNET)
-                .consensus_rpc("https://www.lightclientdata.org")
-                .execution_rpc(&untrusted_rpc_url)
-                .build()?;
+    let mut client = ClientBuilder::new()
+        .network(Network::MAINNET)
+        .consensus_rpc("https://www.lightclientdata.org")
+        .execution_rpc(&untrusted_rpc_url)
+        .build()?;
 
-            client.start().await?;
+    client.start().await?;
 
-            let head_block_num = client.get_block_number().await?;
-            let addr = Address::from_str("Eth_host_address")?;
-            let block = BlockTag::Latest;
-            let balance = client.get_balance(&addr, block).await?;
+    let head_block_num = client.get_block_number().await?;
+    let block = BlockTag::Latest;
+    let balance = client.get_balance(&addr, block).await?;
+    let contract_address: H160 = "your_contract_address".parse()?;
+    let event_signature: H256 = ethers::utils::keccak256("EventName(type1,type2)").into();
 
-            let contract_address = env::var("contract_address")?;
+    println!("synced up to block: {}", head_block_num);
 
-
-            let filter = FilterBuilder::default()
-            .address(vec![contract_address.parse().unwrap()])
-            // You can specify topics (event signatures) here if you want to filter specific events
-            .build();
-    
-        // Subscribe to the filter
-        let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
-        let event = web3.eth_get_logs(filter).await?;
-    
-        println!("Listening for events...");
-
-        fn handle_log(log: Log) {
-            println!("Event: {:?}", log);
-        }
-    
-        sub.for_each(|log| {
-            match log {
-                Ok(log) => handle_log(log),
-                Err(e) => println!("Error: {}", e),
-            }
-    
-            futures::future::ready(())
-        }).await;
-    
-        Ok(());
-
-            println!("synced up to block: {}", head_block_num);
-            println!("balance of deposit contract: {}", utils::format_ether(balance));
-
-            Ok(())
-        });
+    Ok(())
 }
+
+
+
+async fn event_emitted(rpc: &rpc, filter:Filter) -> Result<()> {
+    let logs = rpc.get_logs(filter).await?;
+    let filter = FilterBuilder::default()
+    .address(vec![contract_address])
+    .topics(Some(vec![event_signature]), None, None, None)
+    .build();
+    for log in logs {
+        println!("Log found: {:?}", log);
+        
+    }
+    Ok(())
+}
+
