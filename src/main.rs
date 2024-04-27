d7180d4e6195f0e5ca9e8c5d394e0024dee42e40
Surya M D G <s.gowda@supraoracles.com>
2024-04-27 14:48:40 +0530

    Add batch interval, lantency calculation & execution timeout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use dotenv::dotenv;
-use std::env;
 use std::sync::Arc;
+use std::{env, thread, time};
 use std::time::{SystemTime, UNIX_EPOCH};
 use regex::Regex;
 use hex::FromHex;
@@ -24,11 +24,18 @@ abigen!(
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    let batch_interval_time = 30; // (seconds)
+
     let chain_id: u64 = env::var("DEST_CHAIN_ID")?.parse::<u64>().expect("Invalid conversion to u64");
     let dest_contract_address = env::var("DEST_CONTRACT_ADDR")?.parse::<Address>()?;
 
     let provider = Provider::<Http>::try_from(env::var("DEST_RPC_URL")?.as_str())?;
-    let wallet: Wallet<k256::ecdsa::SigningKey> = env::var("PRIVATE_KEY")?.parse::<Wallet<k256::ecdsa::SigningKey>>()?.with_chain_id(chain_id);
+    // ^ let wallet: Wallet<k256::ecdsa::SigningKey> = env::var("PRIVATE_KEY")?.parse::<Wallet<k256::ecdsa::SigningKey>>()?.with_chain_id(chain_id);
+
+    // CMD line args 
+    let args: Vec<String> = env::args().collect();
+    let wallet: Wallet<k256::ecdsa::SigningKey> = args[1].parse::<Wallet<k256::ecdsa::SigningKey>>()?.with_chain_id(chain_id);
+
     let client = SignerMiddleware::new(provider.clone(), wallet.clone());
 
     let mut log_bytes: Vec<Bytes> = vec![];
@@ -44,13 +51,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut stream = web3.eth_subscribe().subscribe_logs(filter).await?;
 
-    while let Some(Ok(log)) = stream.next().await {
-        //Runs every transaction
-        println!("Received event: {:?}", log);
-        handle_log(&client, &dest_contract_address, &mut log_bytes, log).await?;
-    }
+    let interval = std::time::Duration::from_secs(batch_interval_time);
+    let mut interval = tokio::time::interval(interval);
 
-    Ok(())
+    loop{
+        tokio::select! {
+            log = stream.next() => {
+                if let Some(Ok(log)) = log {
+                    //println!("Received event: {:?}", log);
+                    handle_log(&client, &dest_contract_address, &mut log_bytes, log).await?;
+                }
+            },
+            _ = interval.tick() => {
+                if !log_bytes.is_empty() {
+                    process_log(&client, &dest_contract_address, &mut log_bytes).await?;
+                }
+                else {
+                    println!("~ No Transactions Pending ~\n");
+                }
+            }
+        }
+    }
 }
 
 async fn handle_log(client: &Client, contract_addr: &H160, log_bytes: &mut Vec<Bytes>, log: Log) -> Result<(), Box<dyn std::error::Error>>{
@@ -67,13 +88,14 @@ async fn handle_log(client: &Client, contract_addr: &H160, log_bytes: &mut Vec<B
         let amount = topic3.parse::<Bytes>()?;
 
         //Below code parses event data and calculates curr timestamp - block.timestamp
+
         let data_re = Regex::new(r#"data: Bytes\("0x([^"]+?)"\)"#).unwrap();
         let mut data_str: &str = "";
 
         if let Some(captures) = data_re.captures(log_string.as_str()) {
             data_str = captures.get(1).unwrap().as_str();
         } else {
-            println!("No match found for data_re");
+            println!("handle_log: no match found for data_re");
         }
 
         let action_id_bytes = Vec::from_hex(&topic1[2..]).unwrap();
@@ -92,20 +114,17 @@ async fn handle_log(client: &Client, contract_addr: &H160, log_bytes: &mut Vec<B
         match block_timestamp{
             Some(x) => {
                 match unix_timestamp {
-                    Some(y) => println!("(id: {:?}) Source Contract -> Bridge Node (in seconds): {:?}\n", action_id_val[0], y - x),
-                    None => println!("Invalid current timestamp"),
+                    Some(y) => println!("* (id: {:?}) Source Contract -> Bridge Node (in seconds): {:?}\n", action_id_val[0], y - x),
+                    None => println!("handle_log: invalid current timestamp"),
                 }
             },
-            None => println!("Invalid block.timestamp"),
+            None => println!("handle_log: Invalid block.timestamp"),
         }
 
         //Call pure function to get transaction in bytes form
         get_transaction_bytes(client, contract_addr, action_id, to, amount, log_bytes).await?;
-
-        //TODO: This needs to be run once every interval
-        process_log(client, contract_addr, log_bytes).await?;
     } else {
-        println!("No matches found");
+        println!("handle_log: no matches found");
     }
     //println!("Received event: {:?}", log);
 
@@ -113,23 +132,37 @@ async fn handle_log(client: &Client, contract_addr: &H160, log_bytes: &mut Vec<B
 }
 
 async fn process_log(client: &Client, contract_addr: &H160, log_bytes: &mut Vec<Bytes>) -> Result<(), Box<dyn std::error::Error>> {
+    println!("-----------------------BATCH CALL BEGIN-----------------------\n");
+
     //Call pure function to get final message in bytes form
     let msg = get_message_bytes(client, contract_addr, log_bytes).await?;
 
     //Sign the message
-    let private_key_hex = env::var("PRIVATE_KEY")?;
-    let public_key_hex = env::var("PUBLIC_KEY")?;
+    // ^ let private_key_hex = env::var("PRIVATE_KEY")?;
+    // ^ let public_key_hex = env::var("PUBLIC_KEY")?;
+    
+    let args: Vec<String> = env::args().collect();
+    let private_key_hex = &args[1];
+    let public_key_hex = &args[2];
+    println!("- SIGNER: {}", {public_key_hex});
+
     let sig = ed25519::sign_message(private_key_hex.as_str(), &msg)?;
-    println!("SIG: {}", sig);
+    println!("- SIG: {}", sig);
+
+    //Send to destination contract
+    let timeout: u64 = args[3].parse().unwrap();
+    let ten_millis = time::Duration::from_millis(timeout);
+    thread::sleep(ten_millis);
 
     //Send to destination contract
     execute_message(client, contract_addr, public_key_hex.as_str(), sig, msg).await?;
-
+    
+    println!("------------------------BATCH CALL END------------------------\n");
     Ok(())
 }
 
 async fn get_transaction_bytes(client: &Client, contract_addr: &H160, action_id: Bytes, to: Bytes, amount: Bytes, log_bytes: &mut Vec<Bytes>) -> Result<(), Box<dyn std::error::Error>> {
-    println!("TOPICS: {}, {}, {}", action_id, to, amount);
+    println!("- TOPICS: {}, {}, {}\n", action_id, to, amount);
     
     let contract = BridgeRx::new(contract_addr.clone(), Arc::new(client.clone()));
     let result = contract.get_transaction_bytes(action_id, to, amount).call().await?;
@@ -143,7 +176,7 @@ async fn get_message_bytes(client: &Client, contract_addr: &H160, log_bytes: &mu
     let contract = BridgeRx::new(contract_addr.clone(), Arc::new(client.clone()));
     let result = contract.get_message_bytes(log_bytes.to_vec()).call().await?;
 
-    println!("MSG BYTES: {:?}", result);
+    println!("- MSG BYTES: {:?}", result);
     log_bytes.clear();
 
     Ok(result)
@@ -152,7 +185,7 @@ async fn get_message_bytes(client: &Client, contract_addr: &H160, log_bytes: &mu
 async fn execute_message(client: &Client, contract_addr: &H160, pub_key_hex: &str, sig: Bytes, txn: Bytes) -> Result<(), Box<dyn std::error::Error>> {
     let txn_send_timestamp = get_current_time();
 
-    let pub_key_bytes = Vec::from_hex(pub_key_hex).expect("Invalid hex string");
+    let pub_key_bytes = Vec::from_hex(pub_key_hex).expect("execute_message: Invalid hex string");
     let mut signer: [u8; 32] = [0; 32];
     signer.copy_from_slice(&pub_key_bytes);
 
@@ -168,7 +201,6 @@ async fn execute_message(client: &Client, contract_addr: &H160, pub_key_hex: &st
                     println!("\nTRANSACTION RECEIPT: {}\n", serde_json::to_string(&tx)?);
                     let txn_complete_timestamp = get_current_time();
                     println!("[Transaction completion timestamp] - [Node txn send timestamp] (in ms): {}\n", txn_complete_timestamp - txn_send_timestamp);
-                    println!("-------------------------------------------------------\n");
                 },
                 Err(e) => {
                     println!("TRANSACTION ERROR: {}", e);
